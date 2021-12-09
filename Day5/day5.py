97f8286b0960c7f801a6fcc8ea3c184d22880be6
catarina <catarinabento7828@gmail.com>
2021-12-09 16:41:44 +0000

    ola

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,9 +1,11 @@
 def main():
     f = open("input.txt", "r")
-    lines = f.readlines(); points = []; pointsPassed =  [[[]  for i in range(999)] for i in range(999)]
+    lines = f.readlines(); points = []; pointsPassed =  [[[0]  for i in range(999)] for i in range(999)]
     for i in range(0, len(lines)): points.append(lines[i].strip().split(' ')); points[i].pop(1)
     for x in points:
         for i in range(0,2): x[i] = x[i].split(',')
-        if(x[0][0] != x[1][0] and x[0][1] != x[1][1]): continue
-        for i 
+        if(x[0][0] == x[1][0]): pass
+        elif(x[0][1] == x[1][1]): pass
+        else: continue
+
 main()
