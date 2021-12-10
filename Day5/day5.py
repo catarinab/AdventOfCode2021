6d43e88b81393dd9a1dd5e7f17e5b654c23474a6
Catarina <catarina.c.bento@tecnico.ulisboa.pt>
2021-12-10 16:14:31 +0000

    ya

--- Day5/day5.py
+++ Day5/day5.py
@@ -5,7 +5,12 @@ def main():
     for x in points:
         for i in range(0,2): x[i] = x[i].split(',')
         if(x[0][0] == x[1][0]): pass
-        elif(x[0][1] == x[1][1]): pass
+        elif(x[0][1] == x[1][1]): #9,0 -> 11,0
+            if(int(x[0][0]) > int(x[1][0])):
+                for i in range(int(x[0][0]), int(x[1][0]) + 1): 
+                    pointsPassed[i][0] +=  1
+            elif int(x[0][0]) < int(x[1][0]):
+                    for i in range(int(x[0][0]), int(x[1][0])): pointsPassed[i][int(x[0][1])] +=  1
         else: continue
 
 main()
