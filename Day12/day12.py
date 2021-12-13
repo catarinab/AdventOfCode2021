a0cfe0855afde2711f0c60307f1a742aad33ef31
Catarina <catarina.c.bento@tecnico.ulisboa.pt>
2021-12-13 16:51:45 +0000

    day 12 done

--- Day12/day12.py
+++ Day12/day12.py
@@ -2,60 +2,42 @@ from collections import defaultdict
 class Graph:
     def __init__(self):
         self.graph = defaultdict(list)
-        self.V = 0
+        self.V = self.paths = 0
         self.initialVisitedDict = {}
-        self.paths = 0
-        self.secSmallCave = False
-        self.smallCaveName = ''''''
+        self.smallCaveName = ''
+
+    def verifyStartEnd(self, u, i):
+        return u != 'start' and i != 'start' and u != 'end' and i != 'end'
 
     def addEdge(self, u, v):
         self.graph[u].append(v)
-        self.initialVisitedDict[u] = (False)
-        self.initialVisitedDict[v] = (False)
-
-    def setVertices(self):
-        self.V = len(self.initialVisitedDict)
+        self.graph[v].append(u)
+        self.initialVisitedDict[u] = self.initialVisitedDict[v] = 0
+        self.V +=1
 
     def printAllPaths(self, u, d, visited, path):
-        if(u.islower()):
-            visited[u]= True
+        if(u.islower()): visited[u] +=1
         path.append(u)
 
-        if u == d:
-            self.paths +=1
+        if u == d: self.paths +=1
         else:
             for i in self.graph[u]:
-                print(u, i)
-                if visited[i]== False:
-                    print("Nao visitado")
-                    print(path)
-                    self.printAllPaths(i, d, visited, path)
-                elif self.secSmallCave == False and i.islower() and u != 'start' and i != 'start' and u != 'end' and i != 'end':
-                    self.secSmallCave = True
+                if visited[i] <= 0: self.printAllPaths(i, d, visited, path)
+                elif visited[i] == 1 and i.islower() and self.smallCaveName == '' and self.verifyStartEnd(u, i):
                     self.smallCaveName = i
-                    print("Nao visitado")
-                    print(path)
                     self.printAllPaths(i, d, visited, path)
-                 
-        cave = path.pop()
-        if(cave == self.smallCaveName):
-            print(cave, self.smallCaveName, "self.secSmallCave = False")
-            self.secSmallCave = ''''''
-            self.secSmallCave = False
-        visited[u]= False
+    
+        if(path.pop() == self.smallCaveName): self.smallCaveName = ''
+        visited[u] -=1
 
     def printPaths(self, s, d):
         self.printAllPaths(s, d, self.initialVisitedDict, [])
         print(self.paths)
 
 def main():
-    f = open("inputtest.txt", "r")
-    input = f.readlines(); 
+    input = open("input.txt", "r").readlines()
     for i in range(len(input)): input[i] = input[i].strip().split('-')
     g = Graph()
-    for i in input:
-        g.addEdge(i[0], i[1])
-        g.addEdge(i[1], i[0])
-    g.setVertices()
+    for i in input: g.addEdge(i[0], i[1])
     g.printPaths("start", "end")
 main()
