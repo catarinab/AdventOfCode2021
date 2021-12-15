39d011c03375a1947b2695ceb0478abb177debce
catarina <catarinabento7828@gmail.com>
2021-12-15 21:38:51 +0000

    ya

--- Day14/day14.py
+++ Day14/day14.py
@@ -1,29 +1,23 @@
-import collections
 def part1(templateDict, pairs, steps):
     for step in range(steps):
-        print(step)
-        toAdd = {}; toDelete = []
+        toAdd = {};  finalDict = {}
         for pair in templateDict:
             toAdd[pair] = templateDict[pair]
-            toDelete.append(pair)
+            templateDict[pair] = 0
         for item in toAdd:
             pairToAdd = pairs[item]
             for k in range(2):
                 if(pairToAdd[k] in templateDict): templateDict[pairToAdd[k]] += toAdd[item]
                 else: templateDict[pairToAdd[k]] = toAdd[item]
-                if(pairToAdd[k] in toDelete): toDelete.remove(pairToAdd[k])
-        print(templateDict)
-        for item in toDelete:
-            del templateDict[item]
-        print(templateDict)
-    result = list(templateDict.keys())[0][0]
+    finalDict[list(templateDict.keys())[0][0]] = templateDict[list(templateDict.keys())[0]]
     for pair in templateDict: 
         if(templateDict[pair] > 0):
-            result += pair[1] * templateDict[pair]
-    print(collections.Counter(result).most_common()[0][1] - collections.Counter(result).most_common()[-1][1])
+            if(pair[1] in finalDict): finalDict[pair[1]] += templateDict[pair]
+            else: finalDict[pair[1]] = templateDict[pair]
+    print(finalDict[max(finalDict, key=finalDict.get)] - finalDict[min(finalDict, key=finalDict.get)])
 
 def main():
-    input = open("inputtest.txt", "r").readlines(); pairs = {}; templateDict = {}
+    input = open("input.txt", "r").readlines(); pairs = {}; templateDict = {}
     template = input[0].strip()
     for i in range(2, len(input)):
         result = input[i].strip().split(' -> ')
@@ -31,5 +25,5 @@ def main():
     for i in range(0, len(template) - 1):
         if(template[i]+template[i+1] in templateDict): templateDict[template[i]+template[i+1]] += 1
         else: templateDict[template[i]+template[i+1]] = 1
-    part1(templateDict, pairs, 2)
+    part1(templateDict, pairs, 40)
 main()
